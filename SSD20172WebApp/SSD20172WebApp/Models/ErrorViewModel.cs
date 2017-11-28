15107a1d990874e53d68947853212b82e9684ee1
christianempire <christianempire@hotmail.com>
2017-11-28 13:02:49 -0500

    Solution created

---
+++ SSD20172WebApp/SSD20172WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SSD20172WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
