72f079a96767811ddf6e127d8be221949ce7bc14
christianempire <christianempire@hotmail.com>
2017-12-04 15:10:18 -0500

    Web App finished

--- SSD20172WebApp/SSD20172WebApp/wwwroot/js/site.js
+++ SSD20172WebApp/SSD20172WebApp/wwwroot/js/site.js
@@ -1,2 +1,13 @@
 ﻿// Write your JavaScript code.
-var baseUrl = "http://52.226.131.0/api-arena/";
+//var baseUrl = "http://52.226.131.0/api-arena/";
+var baseUrl = "http://localhost:60650/";
+
+var getSimpleSimulationPayload = function () {
+  return {
+    numExpertAgents: $("#numexp").val(),
+    numNewAgents: $("#numnew").val()
+  }
+};
+
+var getAdvancedSimulationPayload = function () {
+};
